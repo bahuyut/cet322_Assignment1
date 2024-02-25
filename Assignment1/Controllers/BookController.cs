d042628a72d64e556947c92a740ca97718298674
bahuyut <bayramalihuyut99@gmail.com>
2024-02-26 01:51:41 +0300

    Ödev tamamlandı

---
+++ Assignment1/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class BookController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+      
+        [HttpPost]
+        public IActionResult Index(Book book)
+        {
+            if (book == null)
+            {
+                
+                return NotFound();
+            }
+
+            
+            int remainingDays = (int)(book.DueDate - DateTime.Today).TotalDays;
+
+            string color;
+            if (remainingDays < 0)
+                color = "red"; 
+            else if (remainingDays < 3)
+                color = "yellow"; 
+            else
+                color = "green"; 
+
+            ViewBag.RemainingDaysColor = color;
+            ViewBag.RemainingDays = remainingDays;
+
+            return View(book);
+        }
+    }
+}
---
+++ Assignment1/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment1.Models;
+
+public class Book
+{
+    public string? Name { get; set; }
+    public string? Author { get; set; }
+    public int PageNumber { get; set; }
+    public DateTime DueDate { get; set; }
+}
+
+
